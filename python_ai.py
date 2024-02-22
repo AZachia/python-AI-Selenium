e5c2479e68067d4e94e33f46da830ee183d5189c
AZachia <154826103+AZachia@users.noreply.github.com>
2024-02-22 15:44:38 +0100

    update

--- python_ai.py
+++ python_ai.py
@@ -32,8 +32,7 @@ class ChatBot:
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--start-maximized")
 
-        self.driver = webdriver.Chrome(
-            options=chrome_options, service=ChromeService(executable_path=executable_path))
+        self.driver = webdriver.Chrome(options=chrome_options, service=ChromeService(executable_path=executable_path))
         self.driver.get(url)
 
     def promptify(self, prompt):
@@ -48,16 +47,13 @@ class ChatBot:
 
 class youChat(ChatBot):
     def __init__(self, *args, **kwargs):
-        super().__init__("https://you.com/search?q=who+are+you&tbm=youchat&cfr=chat",
-                         view=True, *args, **kwargs)
+        super().__init__("https://you.com/search?q=who+are+you&tbm=youchat&cfr=chat", view=True, *args, **kwargs)
         self.name = 'YouChat'
         time.sleep(1)
 
     def ask(self, prompt: str = 'Who are you ?'):
-        self.driver.find_element(
-            By.ID, 'search-input-textarea').send_keys(prompt)
-        self.driver.find_element(
-            By.ID, 'search-input-textarea').send_keys(Keys.RETURN)
+        self.driver.find_element(By.ID, 'search-input-textarea').send_keys(prompt)
+        self.driver.find_element(By.ID, 'search-input-textarea').send_keys(Keys.RETURN)
         time.sleep(3)
         stop = self.driver.page_source
         while "Stop generating" in stop:
@@ -69,23 +65,18 @@ class youChat(ChatBot):
 class CodeAi(ChatBot):
     def __init__(self, *args, **kwargs):
         super().__init__("https://codepal.ai", *args, **kwargs)
-        self.driver.find_element(
-            By.XPATH, '//*[@id="termsfeed-com---nb"]/div/div[3]/button[2]').click()
+        self.driver.find_element(By.XPATH, '//*[@id="termsfeed-com---nb"]/div/div[3]/button[2]').click()
 
     def set_language(self, langue='python'):
-        self.driver.find_element(
-            By.ID, "select2-select-language-container").click()
-        self.driver.find_element(
-            By.XPATH, "/html/body/span/span/span[1]/input").send_keys(langue)
+        self.driver.find_element(By.ID, "select2-select-language-container").click()
+        self.driver.find_element(By.XPATH, "/html/body/span/span/span[1]/input").send_keys(langue)
         time.sleep(0.5)
-        self.driver.find_element(
-            By.ID, 'select2-select-language-results').click()
+        self.driver.find_element(By.ID, 'select2-select-language-results').click()
 
     def ask(self, prompt=""):
         if prompt not in ["", " ", None]:
             self.driver.find_element(By.ID, 'input-code-description').clear()
-        self.driver.find_element(
-            By.ID, 'input-code-description').send_keys(prompt)
+        self.driver.find_element(By.ID, 'input-code-description').send_keys(prompt)
         self.driver.find_element(By.ID, "btn-generate-code").click()
         time.sleep(10)
         return self.driver.find_element(By.ID, "state-success").text
@@ -97,22 +88,16 @@ class ChatAi(ChatBot):
         self.name = "Chat"
 
     def ask(self, promt):
-        self.driver.find_element(
-            By.XPATH, '//*[@id="scrollContainer"]/div[2]/div/div[1]/input').send_keys(promt)
-        self.driver.find_element(
-            By.XPATH, '//*[@id="scrollContainer"]/div[2]/div/div[1]/input').send_keys(Keys.RETURN)
-        # time.sleep(10)
+        self.driver.find_element(By.XPATH, '//*[@id="scrollContainer"]/div[2]/div/div[1]/input').send_keys(promt)
+        self.driver.find_element(By.XPATH, '//*[@id="scrollContainer"]/div[2]/div/div[1]/input').send_keys(Keys.RETURN)
 
         time.sleep(3)
-        res = len(self.driver.find_elements(
-            By.CLASS_NAME, 'MessageItem_msg__C8oWx')[-1].text)
+        res = len(self.driver.find_elements(By.CLASS_NAME, 'MessageItem_msg__C8oWx')[-1].text)
         time.sleep(1)
         while res < len(self.driver.find_elements(By.CLASS_NAME, 'MessageItem_msg__C8oWx')[-1].text) and self.driver.find_elements(By.CLASS_NAME, 'MessageItem_msg__C8oWx')[-1].text != promt:
             time.sleep(4)
-            res = len(self.driver.find_elements(
-                By.CLASS_NAME, 'MessageItem_msg__C8oWx')[-1].text)
+            res = len(self.driver.find_elements(By.CLASS_NAME, 'MessageItem_msg__C8oWx')[-1].text)
 
-        # return self.driver.find_elements(By.XPATH, '//*[@id="scrollContainer"]/div[1]/div[7]/div[2]/div[1]')[-1].text
         return self.driver.find_elements(By.CLASS_NAME, 'MessageItem_msg__C8oWx')[-1].text
 
 
@@ -128,13 +113,11 @@ class LLMA(ChatBot):
     def retry(self):
         self.driver.find_element(By.ID, "component-9").click()
         time.sleep(3)
-        res = len(self.driver.find_elements(
-            By.XPATH, '//div[@data-testid="bot"]')[-1].text)
+        res = len(self.driver.find_elements(By.XPATH, '//div[@data-testid="bot"]')[-1].text)
         time.sleep(1)
         while res < len(self.driver.find_elements(By.XPATH, '//div[@data-testid="bot"]')[-1].text):
             time.sleep(4)
-            res = len(self.driver.find_elements(
-                By.XPATH, '//div[@data-testid="bot"]')[-1].text)
+            res = len(self.driver.find_elements(By.XPATH, '//div[@data-testid="bot"]')[-1].text)
         return self.driver.find_elements(By.XPATH, '//div[@data-testid="bot"]')[-1].text
 
     def get_respnces(self):
@@ -142,29 +125,17 @@ class LLMA(ChatBot):
 
     def ask(self, prompt: str = 'Who are you ?'):
 
-        prompt = self.role+prompt
-        # print(prompt)
-        self.driver.find_element(
-            By.XPATH, '//*[@id="component-11"]/label/textarea').send_keys(prompt)
-        self.driver.find_element(
-            By.XPATH, '//*[@id="component-11"]/label/textarea').send_keys(Keys.RETURN)
+        self.driver.find_element(By.XPATH, '//*[@id="component-11"]/label/textarea').send_keys(prompt)
+        self.driver.find_element(By.XPATH, '//*[@id="component-11"]/label/textarea').send_keys(Keys.RETURN)
         time.sleep(10)
-        """stop = self.driver.page_source
-        while "Stop generating" in stop:
-            time.sleep(1)
-            stop = self.driver.page_source"""
-        # res = len(self.driver.find_elements(By.XPATH, '//div[@data-testid="bot"]')[-1].text)
-        res = len(self.driver.find_elements(
-            By.XPATH, '//*[@id="component-8"]/div[2]/div[2]/div/div[2]/div')[-1].text)
+        res = len(self.driver.find_elements(By.XPATH, '//*[@id="component-8"]/div[2]/div[2]/div/div[2]/div')[-1].text)
 
         time.sleep(1)
         while res < len(self.driver.find_elements(By.XPATH, '//*[@id="component-8"]/div[2]/div[2]/div/div[2]/div')[-1].text):
             time.sleep(5)
-            res = res = len(self.driver.find_elements(
-                By.XPATH, '//*[@id="component-8"]/div[2]/div[2]/div/div[2]/div')[-1].text)
+            res = res = len(self.driver.find_elements(By.XPATH, '//*[@id="component-8"]/div[2]/div[2]/div/div[2]/div')[-1].text)
 
-        res = res = self.driver.find_elements(
-            By.XPATH, '//*[@id="component-8"]/div[2]/div[2]/div/div[2]/div')[-1].text
+        res = res = self.driver.find_elements(By.XPATH, '//*[@id="component-8"]/div[2]/div[2]/div/div[2]/div')[-1].text
 
         return res
 
@@ -176,30 +147,22 @@ class FreeGPT(ChatBot):
         time.sleep(1)
         self.driver.find_element(By.ID, "ez-accept-all").click()
 
-        # el = self.driver.find_element(By.XPATH, '//*[@id="vnndvnvjlyxpabujuyysueqwdfbwmybthvhpjwfbmunpdejhkbzxxynf"]')
         el = self.driver.find_element(By.XPATH, '/html/body/div[2]')
         self.driver.execute_script("arguments[0].remove()", el)
 
     def ask(self, prompt: str = 'Who are you ?'):
-
-        # print(prompt)
-        self.driver.find_element(
-            By.XPATH, '//*[@id="post-10"]/div/div/div[6]/div/div/div/div/div/div[3]/textarea').send_keys(prompt)
-        self.driver.find_element(
-            By.XPATH, '//*[@id="post-10"]/div/div/div[6]/div/div/div/div/div/div[3]/textarea').send_keys(Keys.RETURN)
+        self.driver.find_element(By.XPATH, '//*[@id="post-10"]/div/div/div[6]/div/div/div/div/div/div[3]/textarea').send_keys(prompt)
+        self.driver.find_element(By.XPATH, '//*[@id="post-10"]/div/div/div[6]/div/div/div/div/div/div[3]/textarea').send_keys(Keys.RETURN)
         time.sleep(5)
 
-        res = len(self.driver.find_elements(
-            By.CLASS_NAME, 'wpaicg-ai-message')[-1].text)
+        res = len(self.driver.find_elements(By.CLASS_NAME, 'wpaicg-ai-message')[-1].text)
 
         time.sleep(1)
         while res < len(self.driver.find_elements(By.CLASS_NAME, 'wpaicg-ai-message')[-1].text):
             time.sleep(5)
-            res = res = len(self.driver.find_elements(
-                By.CLASS_NAME, 'wpaicg-ai-message')[-1].text)
+            res = res = len(self.driver.find_elements(By.CLASS_NAME, 'wpaicg-ai-message')[-1].text)
 
-        res = res = self.driver.find_elements(
-            By.CLASS_NAME, 'wpaicg-ai-message')[-1].text
+        res = res = self.driver.find_elements(By.CLASS_NAME, 'wpaicg-ai-message')[-1].text
 
         return res
 
@@ -212,14 +175,11 @@ class EChat(ChatBot):
 
     def ask(self, prompt: str = 'Who are you ?'):
 
-        self.driver.find_element(
-            By.CLASS_NAME, 'chatview__textarea-message').send_keys(prompt)
-        self.driver.find_element(
-            By.CLASS_NAME, 'chatview__textarea-message').send_keys(Keys.RETURN)
+        self.driver.find_element(By.CLASS_NAME, 'chatview__textarea-message').send_keys(prompt)
+        self.driver.find_element(By.CLASS_NAME, 'chatview__textarea-message').send_keys(Keys.RETURN)
         time.sleep(10)
 
-        res = len(self.driver.find_elements(
-            By.CLASS_NAME, 'flex-row-reverse')[-1].text)
+        res = len(self.driver.find_elements(By.CLASS_NAME, 'flex-row-reverse')[-1].text)
 
         time.sleep(1)
         while res < len(self.driver.find_elements(By.CLASS_NAME, 'flex-row-reverse')[-1].text):
@@ -241,31 +201,25 @@ class MyGPT(ChatBot):
 
     def ask(self, prompt: str = 'Who are you ?'):
 
-        # print(prompt)
-        self.driver.find_element(
-            By.XPATH, '//*[@id="post-10"]/div/div/div[6]/div/div/div/div/div/div[3]/textarea').send_keys(prompt)
-        self.driver.find_element(
-            By.XPATH, '//*[@id="post-10"]/div/div/div[6]/div/div/div/div/div/div[3]/textarea').send_keys(Keys.RETURN)
+        self.driver.find_element(By.XPATH, '//*[@id="post-10"]/div/div/div[6]/div/div/div/div/div/div[3]/textarea').send_keys(prompt)
+        self.driver.find_element(By.XPATH, '//*[@id="post-10"]/div/div/div[6]/div/div/div/div/div/div[3]/textarea').send_keys(Keys.RETURN)
         time.sleep(5)
 
-        res = len(self.driver.find_elements(
-            By.CLASS_NAME, 'wpaicg-ai-message')[-1].text)
+        res = len(self.driver.find_elements(By.CLASS_NAME, 'wpaicg-ai-message')[-1].text)
 
         time.sleep(1)
         while res < len(self.driver.find_elements(By.CLASS_NAME, 'wpaicg-ai-message')[-1].text):
             time.sleep(5)
-            res = res = len(self.driver.find_elements(
-                By.CLASS_NAME, 'wpaicg-ai-message')[-1].text)
+            res = res = len(self.driver.find_elements(By.CLASS_NAME, 'wpaicg-ai-message')[-1].text)
 
-        res = res = self.driver.find_elements(
-            By.CLASS_NAME, 'wpaicg-ai-message')[-1].text
+        res = res = self.driver.find_elements(By.CLASS_NAME, 'wpaicg-ai-message')[-1].text
 
         return res
 
 
 if __name__ == '__main__':
-    chat = FreeGPT()
 
+    chat = FreeGPT()
 
     command = input(">")
     while command not in ("q", "quit", "exit"):
